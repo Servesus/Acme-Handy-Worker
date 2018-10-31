1554e39b5c96b42c0757fe924eb420f454a5baf7
Servesus <jesusgarrocho1998@gmail.com>
2018-10-31 01:33:11 +0100

    Mas Clases

--- src/main/java/domain/Configuration.java
+++ src/main/java/domain/Configuration.java
@@ -1,8 +1,41 @@
+
 package domain;
 
+import java.util.Date;
+
+import org.hibernate.validator.constraints.Range;
 
 public class Configuration extends DomainEntity {
+
 	//Attributes
-	private 
+	private int		maxResults	= 10;
+	private int		maxTime		= 1;
+	private Date	lastUpdate;
+
+
+	@Range(min = 10, max = 100)
+	public int getMaxResults() {
+		return this.maxResults;
+	}
+
+	public void setMaxResults(final int maxResults) {
+		this.maxResults = maxResults;
+	}
+	@Range(min = 1, max = 24)
+	public int getMaxTime() {
+		return this.maxTime;
+	}
+
+	public void setMaxTime(final int maxTime) {
+		this.maxTime = maxTime;
+	}
+
+	public Date getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	public void setLastUpdate(final Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
 
 }
