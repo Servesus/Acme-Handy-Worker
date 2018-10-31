53e3378328e3fdc2a126657d409a9fe29c4b9c0b
sergioperez1998 <sergio-pm@hotmail.com>
2018-10-31 12:10:33 +0100

    Clases con relaciones terminadas

--- src/main/java/domain/Complain.java
+++ src/main/java/domain/Complain.java
@@ -4,19 +4,29 @@ package domain;
 import java.util.Collection;
 import java.util.Date;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
+@Access(AccessType.PROPERTY)
 public class Complain extends DomainEntity {
 
-	private String				ticker;
-	private Date				moment;
-	private String				description;
-	private Collection<String>	attchment;
+	private String					ticker;
+	private Date					moment;
+	private String					description;
+	private Collection<String>		attchment;
+
+	//Relationships:
+	private Customer				customer;
+	private Collection<Report>		reports;
+	private Collection<Fix_up_Task>	fix_up_tasks;
 
 
 	@NotBlank
@@ -53,6 +63,36 @@ public class Complain extends DomainEntity {
 
 	public void setAttchment(final Collection<String> attchment) {
 		this.attchment = attchment;
+	}
+
+	//Relationships:
+
+	@NotNull
+	@Valid
+	public Customer getCustomer() {
+		return this.customer;
+	}
+
+	public void setCustomer(final Customer customer) {
+		this.customer = customer;
+	}
+
+	@Valid
+	public Collection<Report> getReports() {
+		return this.reports;
+	}
+
+	public void setReports(final Collection<Report> reports) {
+		this.reports = reports;
+	}
+
+	@Valid
+	public Collection<Fix_up_Task> getFix_up_tasks() {
+		return this.fix_up_tasks;
+	}
+
+	public void setFix_up_tasks(final Collection<Fix_up_Task> fix_up_tasks) {
+		this.fix_up_tasks = fix_up_tasks;
 	}
 
 }
